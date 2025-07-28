3339f163eece2014ef3091f5598a2931b8ba95a6
chrisjamhol <chrisjamhol@gmail.com>
2025-07-28 16:38:22 +0200

    init svelt

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let testcounter = $state(0);
+	function greet() {
+		testcounter++;
+		console.log("hi chrissa");
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
+	the documentation
+</p>
+<button onclick={greet}>testing {testcounter} - count</button>
+
+<style>
+	button {
+		background: green;
+		color: white;
+	}
+</style>
